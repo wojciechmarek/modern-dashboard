37369b6c58908d86b523d057032175570944e89e
Wojciech Marek <marek.wojciech@outlook.com>
2023-01-31 19:54:13 +0100

    feat: add swagger to the web app

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,15 +5,28 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Employee Dashboard')
+    .setDescription('The employee dashboard API')
+    .setVersion('1.0')
+    .addTag('employee dashboard')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
