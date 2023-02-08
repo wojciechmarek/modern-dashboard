4e5a059b7642fddd56c446ef0e9a22f72242b29b
Wojciech Marek <marek.wojciech@outlook.com>
2023-02-08 23:44:39 +0100

    feat: add extendable mange button

---
+++ libs/web/assets/src/images/User/index.ts
@@ -0,0 +1,1 @@
+export { default as UserImage } from './user.jpg';
--- libs/web/views/src/Features/Navbar/Navbar.svelte
+++ libs/web/views/src/Features/Navbar/Navbar.svelte
@@ -4,11 +4,18 @@
     BarsThreeIcon,
     LogoImage,
     ChevronDownIcon,
+    UserImage,
   } from '@employee-dashboard/web/assets';
+
+  let manageOpen: boolean;
+
+  function toggle() {
+    manageOpen = !manageOpen;
+  }
 </script>
 
 <nav
-  class="h-16 bg-gray-200 dark:bg-gray-800 flex justify-between items-center px-3"
+  class="h-16 bg-gray-200 dark:bg-gray-800 flex justify-between items-center pl-3 pr-5 md:px-16"
 >
   <div class="h-10 w-10 md:hidden">
     <Button class="hover:bg-gray-700">
@@ -23,17 +30,39 @@
       <img src={LogoImage} alt="logo" class="h-8" />
       <p class="text-gray-200 uppercase text-2xl font-extrabold">MicroChip</p>
     </div>
-    <div class="h-10 w-fit">
-      <Button class="hover:bg-gray-700 mx-3 flex justify-center items-center">
-        <p class="text-gray-200">Manage</p>
-        <Icon icon={ChevronDownIcon} class="h-6 w-6" />
+    <div class="h-10 w-fit relative">
+      <Button class="group mx-3 flex justify-center items-center">
+        <p class="text-gray-200 group-hover:underline" on:click={toggle}>
+          Manage
+        </p>
+        <Icon icon={ChevronDownIcon} class="group-hover:rotate-180 h-6 w-6" />
       </Button>
+      {#if manageOpen}
+        <dialog open class="h-40 w-72 bg-gray-200 absolute top-12">
+          <ul>
+            <li class="hover:bg-slate-500">
+              <a href="/manage">Manage</a>
+            </li>
+            <li class="hover:bg-slate-500">
+              <a href="/manage">Manage</a>
+            </li>
+            <li class="hover:bg-slate-500">
+              <a href="/manage">Manage</a>
+            </li>
+            <li class="hover:bg-slate-500">
+              <a href="/manage">Manage</a>
+            </li>
+            <li class="hover:bg-slate-500">
+              <a href="/manage">Manage</a>
+            </li>
+          </ul>
+        </dialog>
+      {/if}
     </div>
   </div>
 
-  <ul class="flex justify-center gap-2">
-    <li><a href="/">Dashboard</a></li>
-    <li><a href="/employees">Employees</a></li>
-    <li><a href="/projects">Projects</a></li>
-  </ul>
+  <div class="flex justify-center items-center gap-4 cursor-pointer">
+    <p class="text-gray-200 text-1xl font-bold">Steven</p>
+    <img src={UserImage} alt="avatar" class="h-8 w-8 rounded-full" />
+  </div>
 </nav>
