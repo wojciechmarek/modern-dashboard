91e04ee4652bad881c0a29ac5b694ed7fb590581
Wojciech Marek <marek.wojciech@outlook.com>
2023-03-21 15:30:01 +0100

    chore: remove svelte app project

--- apps/web/svelte.config.cjs
+++
@@ -1,7 +0,0 @@
-const sveltePreprocess = require('svelte-preprocess');
-
-module.exports = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: sveltePreprocess(), // can pass the options here using { }, but we use svelte.preprocess.config.js for that (gives us more context)
-};
