f6c9f8c018b2168fcab57ee16b69c6e3f1041a06
Wojciech Marek <marek.wojciech@outlook.com>
2023-03-26 17:28:02 +0200

    feat: add colors theme change button

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'md-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web';
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+
+  }
+
+  isDark = false;
+
+  themeToggle() {
+    if (this.isDark) {
+      document.body.classList.remove('light');
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+      document.body.classList.add('light');
+    }
+
+    this.isDark = !this.isDark;
+  };
 }
