d92a9d1b959b479b1b55ca6332b4e392db1fc5cf
Wojciech Marek <marek.wojciech@outlook.com>
2023-04-01 14:03:43 +0200

    feat: restore login styles

--- libs/web/components/organisms/src/lib/login/login.component.ts
+++ libs/web/components/organisms/src/lib/login/login.component.ts
@@ -5,7 +5,4 @@ import { Component } from '@angular/core'
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
-
-
-}
+export class LoginComponent { }
