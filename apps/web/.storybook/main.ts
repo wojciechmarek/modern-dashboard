2524ed24b9eed53ba24d4671927d9e2cd22195cf
Wojciech Marek <marek.wojciech@outlook.com>
2023-04-04 12:18:37 +0200

    chore: add storybook configuration files with TS flag

---
+++ apps/web/.storybook/main.ts
@@ -0,0 +1,16 @@
+import type { StorybookConfig } from '@storybook/core-common';
+
+const config: StorybookConfig = {
+  core: { builder: 'webpack5' },
+  stories: [
+    '../src/app/**/*.stories.mdx',
+    '../src/app/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: ['@storybook/addon-essentials'],
+};
+
+module.exports = config;
+
+// To customize your webpack configuration you can use the webpackFinal field.
+// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
+// and https://nx.dev/packages/storybook/documents/custom-builder-configs
