3e7f3f9b2cf67422b05a146f6c9eabcc73b7297e
Wojciech Marek <marek.wojciech@outlook.com>
2023-04-09 13:28:35 +0200

    feat: add redux store tools

--- libs/web/common/store/src/lib/web-common-store.module.ts
+++ libs/web/common/store/src/lib/web-common-store.module.ts
@@ -3,11 +3,16 @@ import { StoreModule } from '@ngrx/store';
 
 import { authReducer } from './auth';
 import { themeReducer } from './theme';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 
 @NgModule({
   imports: [
     StoreModule.forRoot({ theme: themeReducer, auth: authReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: true, //process.env.PRODUCTION === "true", // Restrict extension to log-only mode
+    }),
   ],
 })
 export class WebCommonStoreModule {}
