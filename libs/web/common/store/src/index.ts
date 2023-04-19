f78b36044902481246fb0a0ea3753af944686103
Wojciech Marek <marek.wojciech@outlook.com>
2023-04-19 12:29:48 +0200

    feat: remove exports to another index.ts file

--- libs/web/common/store/src/index.ts
+++ libs/web/common/store/src/index.ts
@@ -1,3 +1,1 @@
-export * from './lib/web-common-store.module';
-export * from './lib/auth/auth.actions';
-export * from './lib/profile/profile.actions';
+export * from './lib';
---
+++ libs/web/common/store/src/lib/index.ts
@@ -0,0 +1,3 @@
+export * from './auth/auth.actions';
+export * from './profile/profile.actions';
+export * from './web-common-store.module';
