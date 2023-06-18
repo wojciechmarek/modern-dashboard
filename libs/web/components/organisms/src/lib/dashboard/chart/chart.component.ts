a3ee2958a8c8c06dac5827bc4fc81605613e88c6
Wojciech Marek <marek.wojciech@outlook.com>
2023-06-18 14:01:32 +0200

    feat: add fake bars and charts

--- libs/web/components/organisms/src/lib/dashboard/chart/chart.component.ts
+++ libs/web/components/organisms/src/lib/dashboard/chart/chart.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'md-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css'],
 })
-export class ChartComponent {}
+export class ChartComponent {
+  @Input() title = '';
+  @Input() imageSrc = '';
+  @Input() value = '';
+  @Input() description = '';
+  @Input() trendText = '';
+  @Input() trendColor = '';
+}
--- libs/web/components/organisms/src/lib/dashboard/worktime/worktime.component.ts
+++ libs/web/components/organisms/src/lib/dashboard/worktime/worktime.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'md-worktime',
