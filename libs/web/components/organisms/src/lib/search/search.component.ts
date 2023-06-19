6ebffb7563c8a71dd701794cd48102e21b40009d
Wojciech Marek <marek.wojciech@outlook.com>
2023-06-19 19:06:02 +0200

    feat: add styles to search bar

--- libs/web/components/organisms/src/lib/search/search.component.ts
+++ libs/web/components/organisms/src/lib/search/search.component.ts
@@ -1,20 +1,28 @@
-import { Component, ElementRef, EventEmitter, HostListener, Output, ViewChild, inject } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, OnInit, Output, ViewChild, inject } from '@angular/core';
 
 @Component({
   selector: 'md-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit{
   @ViewChild('searchContainer') searchContainer: ElementRef | undefined;
+  @ViewChild('searchInput') searchInput: ElementRef | undefined;
 
   @Output() handleCloseSearch = new EventEmitter();
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.searchInput?.nativeElement.focus();
+    }, 0);
+  }
 
   closeSearch() {
     this.handleCloseSearch.emit();
   }
 
   @HostListener('document:keydown.esc')
+  @HostListener('document:keydown.meta.k')
   handleKeyboardEvent() { 
     this.handleCloseSearch.emit();
   }
