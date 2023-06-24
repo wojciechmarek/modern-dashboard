f6446051013505a4b4145e4ff77b7d93399020cd
Wojciech Marek <marek.wojciech@outlook.com>
2023-06-24 21:07:26 +0200

    feat: add responsiveness to the dashboard

--- libs/web/common/assets/src/index.ts
+++ libs/web/common/assets/src/index.ts
@@ -1,1 +1,1 @@
-export * from './lib/web-assets';
+
--- libs/web/components/atoms/src/lib/web-components-atoms.module.ts
+++ libs/web/components/atoms/src/lib/web-components-atoms.module.ts
@@ -1,24 +1,24 @@
-import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
-import { RouterModule } from '@angular/router'
-import { ButtonComponent } from './button/button.component'
-import { TextComponent } from './text/text.component'
-import { IconComponent } from './icon/icon.component'
-import { HeaderComponent } from './header/header.component'
-import { ReactiveFormsModule } from '@angular/forms'
-import { TextInputComponent } from './text-input/text-input.component'
-import { CheckboxComponent } from './checkbox/checkbox.component'
-import { LinkComponent } from './link/link.component'
-import { HorizontalLineComponent } from './horizontal-line/horizontal-line.component'
-import { HorizontalSelectorComponent } from './horizontal-selector/horizontal-selector.component'
-import { TitleComponent } from './title/title.component'
-import { ThemeSelectorComponent } from './theme-selector/theme-selector.component'
-import { FlatLinkComponent } from './flat-link/flat-link.component'
-import { LanguageSelectorComponent } from './language-selector/language-selector.component'
-import { LucideAngularModule, ChevronDown } from 'lucide-angular'
-import { SelectComponent } from './select/select.component'
-import { SwitchComponent } from './switch/switch.component'
-import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component'
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ButtonComponent } from './button/button.component';
+import { TextComponent } from './text/text.component';
+import { IconComponent } from './icon/icon.component';
+import { HeaderComponent } from './header/header.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TextInputComponent } from './text-input/text-input.component';
+import { CheckboxComponent } from './checkbox/checkbox.component';
+import { LinkComponent } from './link/link.component';
+import { HorizontalLineComponent } from './horizontal-line/horizontal-line.component';
+import { HorizontalSelectorComponent } from './horizontal-selector/horizontal-selector.component';
+import { TitleComponent } from './title/title.component';
+import { ThemeSelectorComponent } from './theme-selector/theme-selector.component';
+import { FlatLinkComponent } from './flat-link/flat-link.component';
+import { LanguageSelectorComponent } from './language-selector/language-selector.component';
+import { LucideAngularModule, ChevronDown } from 'lucide-angular';
+import { SelectComponent } from './select/select.component';
+import { SwitchComponent } from './switch/switch.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 
 const Components = [
   ButtonComponent,
@@ -36,11 +36,16 @@ const Components = [
   LanguageSelectorComponent,
   SelectComponent,
   SwitchComponent,
-  BreadcrumbComponent
+  BreadcrumbComponent,
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule, ReactiveFormsModule, LucideAngularModule.pick({ ChevronDown })],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    LucideAngularModule.pick({ ChevronDown }),
+  ],
   declarations: [...Components],
   exports: [...Components],
 })
