859153197eab35d3507349c1f524187d66928afd
Wojciech Marek <marek.wojciech@outlook.com>
2023-12-11 15:13:51 +0100

    feat: add translation to dashboard

--- libs/web/feature/dashboard/src/lib/web-views-dashboard.module.ts
+++ libs/web/feature/dashboard/src/lib/web-views-dashboard.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
 import { LucideAngularModule, Calendar } from 'lucide-angular';
 import { WebComponentsOrganismsModule } from '@md/web/ui/organisms';
+import { TranslateModule } from '@ngx-translate/core';
 
 const routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
   imports: [
     CommonModule,
+    TranslateModule,
     RouterModule.forChild(routes),
     LucideAngularModule.pick({ Calendar }),
     WebComponentsOrganismsModule,
